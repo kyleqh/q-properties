22b7505f795380f206b0c1bd0d43970c855b74ef
kyleqh <kyle.grovers@qantashotels.com>
2024-10-02 06:09:22 +1000

    move savings display logic and fix styling for display price

--- src/app/property/components/RoomRate.tsx
+++ src/app/property/components/RoomRate.tsx
@@ -1,26 +1,33 @@
 import { Currency, Money } from "../types";
 
-const Savings = ({amount}: { amount: number}) => {
+const Savings = ({amount}: { amount: number | null | undefined}) => {
 
   return (
-    <div className="text-red-400 text-sm font-semibold">
-      {`Save $${amount}`}
-      <span className="align-top">
-        ~
-      </span>
+    <div className="text-red-400 text-sm font-semibold pb-1">
+      { amount ?
+      (
+        <div>
+          {`Save $${amount}`}
+          <span className="align-top">
+            ~
+          </span>
+        </div>
+      )
+        : <div className="h-6" />
+      }
     </div>
   )
 }
 
 export const RoomRate = ({ displayPrice, savings }: { displayPrice: Money , savings: Money | null }) => {
   return (
-    <div className="basis-1/5 border border-gray-600 text-right pt-30">
+    <div className="basis-1/5 border border-gray-600 text-right content-end">
       <div className="text-xs text-gray-700">{`1 night total (${ Currency.AUD})`}</div>
       <div>
         <span className="align-top">$</span>
         <span className="text-3xl font-semibold">{displayPrice.amount}</span>
       </div>
-      { savings?.amount ? (<Savings amount={savings.amount} />) : null }
+      <Savings amount={savings?.amount} />
     </div>
   )
 }
