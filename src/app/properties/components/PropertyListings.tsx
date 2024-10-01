72aeca3a807cb68d48ba85e00104254d3d1fd8b2
kyleqh <kyle.grovers@qantashotels.com>
2024-10-02 09:36:00 +1000

    change favicon and update some linting errors

--- src/app/properties/components/PropertyListings.tsx
+++ src/app/properties/components/PropertyListings.tsx
@@ -10,7 +10,7 @@ import { RoomRate } from "./RoomRate"
 
 const PropertyListing = ({ listing } : { listing : Listing }) => {
   return (
-    <div className="flex flex-row gap-4">
+    <div className="flex flex-row gap-2">
       <Preview previewImage={listing.property.previewImage} promoTitle={listing.offer.promotion.title} />
       <PropertyInfo property={listing.property} offer={listing.offer} />
       <RoomRate displayPrice={listing.offer.displayPrice} savings={listing.offer.savings} />
@@ -27,7 +27,7 @@ const PropertyCountSlug = ({count }:{count: number}) => (
 )
 
 const SortDropDown = ({sortList} : {sortList : (e : any) => void}) => (
-  <div className="ml-auto content-end pr-96">
+  <div className="ml-auto content-end">
   <span className="font-bold">Sort by &nbsp;</span>
   <select className="rounded border border-gray-200" onChange={(e) => sortList(e)}>
     <option value='hl'>Price high-low</option>
@@ -45,7 +45,7 @@ export const PropertyListings = ({ listings } : { listings: Listing[] }) => {
     LOWTOHIGH='lh'
   }
 
-  const sortList = e => {
+  const sortList = (e: { target: { value: any } }) => {
     const sortOption = e.target.value
     const sorter = {
       [PriceSort.HIGHTOLOW]: () : Listing[] => listingData.sort((a, b) => b.offer.displayPrice.amount - a.offer.displayPrice.amount),
